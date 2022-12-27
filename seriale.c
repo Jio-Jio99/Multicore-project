f324951a0a14de8fbdb19e6b198dd51614b2587e
Gioele <zoccoli.1850491@studenti.uniroma1.it>
2022-12-27 19:24:24 +0100

    fix serial

--- seriale.c
+++ seriale.c
@@ -3,16 +3,30 @@
 #include <stdlib.h>
 #include "utils.c"
 
-// Driver program to test above function
+// Main per il processo seriale
 int main(int argc, char* args[]) {
-    char* txt = ReadFile("gioele.txt");
+    char* filenameTxt = "gioele.txt";
+    char* filenamePath = "gioele.txt";
+
+    // Leggo i file e metto in memoria, controllando che non ci siano errori
+    char* txt = ReadFile(filenameTxt);
     if (txt == NULL){
-        printf("Errore nella lettura del file");
+        printf("Error: file of TEXT not exist");
         return 0;
     }
 
-    char* pat = "Dix";
+    char* pat = ReadFile(filenamePath);
+    if (pat == NULL){
+        printf("Error: file of PAT not exist");
+        return 0;
+    }
 
-    KMPSearch(pat, txt);
+    // Del file path splitto sugli '\n' per poter successivamente ciclare sulle stringhe da verificare
+    char** listaPat = StrSplit(pat, '\n');
+
+    // Funzionamento del processo seriale, per ogni token avvio la funzione
+    for(int i=0; *(listaPat + i); i++){
+        KMPSearch(listaPat[i], txt);
+    }
     return 0;
 }
