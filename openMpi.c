0a7ce52150197ebab2d680e7da81983b2586c54f
Jio-Jio <zoccoli.1850491@studenti.uniroma1.it>
2022-12-28 17:09:26 +0100

    fix file

--- openMpi.c
+++ openMpi.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <omp.h>
 #include "utils.c"
+
+
+int main(int argc, char* agrs[]){
+
+    return 0;
+}
+
+
 
 // RITORNARE UN BOOLEANO SE TROVA QUELLA STRINGA
 void OpenMPI(){
