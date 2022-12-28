028fc6a135f13e80ff63210d4a12e89e35167057
Gioele <zoccoli.1850491@studenti.uniroma1.it>
2022-12-28 10:22:14 +0100

    add file for text and path + check on args

--- seriale.c
+++ seriale.c
@@ -5,8 +5,13 @@
 
 // Main per il processo seriale
 int main(int argc, char* args[]) {
-    char* filenameTxt = "gioele.txt";
-    char* filenamePath = "gioele.txt";
+    if (argc != 3) {
+        printf("Error: errore nei parametri passati\n");
+        return 0;
+    } 
+
+    char* filenameTxt = args[1];
+    char* filenamePath = args[2];
 
     // Leggo i file e metto in memoria, controllando che non ci siano errori
     char* txt = ReadFile(filenameTxt);
@@ -26,6 +31,7 @@ int main(int argc, char* args[]) {
 
     // Funzionamento del processo seriale, per ogni token avvio la funzione
     for(int i=0; *(listaPat + i); i++){
+        printf("\nParola in ricerca: %s\n", *(listaPat+i));
         KMPSearch(listaPat[i], txt);
     }
     return 0;
