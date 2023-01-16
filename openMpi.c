9dd2b533dd4dd7d8f6d0e82daf1b51c2833572e8
Jio-Jio99 <zoccoli.1850491@studenti.uniroma1.it>
2023-01-16 14:57:34 +0100

    add comment

--- openMpi.c
+++ openMpi.c
@@ -6,57 +6,64 @@
 #include "utils.c"
 
 int main(int argc, char* args[]){
+    //Controllo che i parametri passati siano del numero esatto, il IV è il numero di processi paralleli da avviare
     if (argc != 4) {
         printf("Error: mancano dei parametri\n");
         return 0;
     } 
 
+    //Prendo i nomi dei file
     char* filenameTxt = args[1];
-    char* filenamePath = args[2];
+    char* filenamePatt = args[2];
+    //Prendo il numero di processi
     int thread_count = atoi(args[3]);
 
+    //Controllo che non ci siano stati errori nella conversione
     if (thread_count == 0){
         printf("Error: input tread count with a integer");
         return 0;
     }
 
-    // Leggo i file e metto in memoria, controllando che non ci siano errori
+    // Leggo i file e alloco una parte di memoria dedicata, controllando che non ci siano errori
+    //File TESTO
     char* txt = ReadFile(filenameTxt);
     if (txt == NULL){
         printf("Error: file of TEXT not exist");
         return 0;
     }
 
-    char* pat = ReadFile(filenamePath);
+    //File dei pattern
+    char* pat = ReadFile(filenamePatt);
     if (pat == NULL){
         printf("Error: file of PAT not exist");
         return 0;
     }
 
-    // Del file path splitto sugli '\n' per poter successivamente ciclare sulle stringhe da verificare
-    int numberPat = 0;
+    //Del file patt splitto sugli '\n' per poter successivamente ciclare sulle stringhe da verificare
+    int numberPat = 0; //Mi salvo quanti pattern ha trovato nel file così da poter avere una costante nel for
     char** listaPat = StrSplit(pat, '\n', &numberPat);
 
-    // Idea: ciclare sui pattern e ad ogni pattern mandare un thread
+    // Idea: ciclare sui pattern mandando in parallelo il for, quindi suddividere i pattern da cercare tra i vari processi
+    //Misurazione per il tempo
     double start, end;
     start = omp_get_wtime();
     #pragma  omp  parallel num_threads(thread_count)
     {
-        int volte = 0; 
+        int volte = 0;  //Variabile privata dei vari thread che serve per calcolare quante volte è stata trovata la parola
         #pragma omp for private(volte) 
+            //For dei pattern che va da 0 a numberPat -1 
             for (int i = 0; i < numberPat; i++){
                 if (!listaPat[i]){
-                    continue;
+                    i = numberPat;
+                    continue;//Controllo in più per verificare se non si va in una parte di memoria vuota, altrimenti continua con il ciclo per poi uscire
                 }
                 volte = 0; 
-                KMPSearchInt(listaPat[i], txt, &volte);
-                // KMPSearch(listaPat[i], txt);
+                KMPSearchInt(listaPat[i], txt, &volte); //Algoritmo che cerca le ricorrenze nei testi
                 printf("Ricercato \"%s\" dal processo %d di %d: %d ricorenze trovate\n", listaPat[i], omp_get_thread_num(),omp_get_num_threads(),volte);
             }
     }
     end = omp_get_wtime();
+    //stampa finale del tempo messo
     printf("\nTime: %f\n", end - start);
     return 0;
 }
-
-//Fare più txt copie profonda (anche della lista de)
--- seriale.c
+++ seriale.c
@@ -8,22 +8,26 @@ void serialeFunction(char *testo, char** pattern);
 
 // Main per il processo seriale
 int main(int argc, char* args[]) {
+    //Controllo che i parametri passati siano del numero esatto, il IV è il numero di processi paralleli da avviare
     if (argc != 3) {
         printf("Error: errore nei parametri passati\n");
         return 0;
     } 
 
+    //Prendo i nomi dei file
     char* filenameTxt = args[1];
-    char* filenamePath = args[2];
+    char* filenamePatt = args[2];
 
     // Leggo i file e metto in memoria, controllando che non ci siano errori
+    //File del testo da controllare
     char* txt = ReadFile(filenameTxt);
     if (txt == NULL){
         printf("Error: file of TEXT not exist");
         return 0;
     }
 
-    char* pat = ReadFile(filenamePath);
+    //File dei pattern da cercare
+    char* pat = ReadFile(filenamePatt);
     if (pat == NULL){
         printf("Error: file of PAT not exist");
         return 0;
@@ -32,9 +36,11 @@ int main(int argc, char* args[]) {
     // Del file path splitto sugli '\n' per poter successivamente ciclare sulle stringhe da verificare
     char** listaPat = StrSplit(pat, '\n', NULL);
 
-    // Funzionamento del processo seriale, per ogni token avvio la funzione
+    // Funzionamento del processo seriale, per ogni pattern avvio la funzione di ricerca
+    //Calcolo dei tempi
     clock_t start, end;
     start = clock();
+    //Funzione seriale
     serialeFunction(txt, listaPat);
     end = clock();
     printf("\nTime: %f\n", ((double)(end - start))/CLOCKS_PER_SEC);
@@ -43,11 +49,11 @@ int main(int argc, char* args[]) {
 
 void serialeFunction(char *testo, char** pattern){
     int volte;
+    //Ciclo su tutti i pattern dati finchè l'array non è vuoto
     for(int i=0; *(pattern + i); i++){
         volte = 0;
         printf("Trovata la parola \"%s\"", *(pattern+i));
-        KMPSearchInt(pattern[i], testo, &volte);
-        // KMPSearch(pattern[i], testo);
+        KMPSearchInt(pattern[i], testo, &volte); //Algoritmo che calcola le ricorrenze della parola data
         printf(" %d volte\n", volte);
     }
 
