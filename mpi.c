629956a5617cdf0d8b9efbb7c788d490ef26734f
Jio-Jio99 <zoccoli.1850491@studenti.uniroma1.it>
2023-01-22 15:53:01 +0100

    function mpi added

--- mpi.c
+++ mpi.c
@@ -2,28 +2,21 @@
 #include <string.h>
 #include <stdlib.h>
 #include <time.h>
-#include <omp.h>
+#include <mpi.h>
 #include "utils.c"
 
 int main(int argc, char* args[]){
     //Inizializzazione e pars dei parametri
     //Controllo che i parametri passati siano del numero esatto, il IV è il numero di processi paralleli da avviare
-    if (argc != 6) {
+    if (argc != 3) {
         printf("Error: mancano dei parametri\n");
         return 0;
     } 
 
     //Prendo i nomi dei file (sono gli ultimi inseriti)
-    char* filenameTxt = args[4];
-    char* filenamePatt = args[5];
-    //Prendo il numero di processi
-    int comm_sz = atoi(args[3]);
+    char* filenameTxt = args[1];
+    char* filenamePatt = args[2];
 
-    //Controllo che non ci siano stati errori nella conversione
-    if (comm_sz == 0){
-        printf("Error: input tread count with a integer");
-        return 0;
-    }
 
     // Leggo i file e alloco una parte di memoria dedicata, controllando che non ci siano errori
     //File TESTO
@@ -45,22 +38,33 @@ int main(int argc, char* args[]){
     char** listaPat = StrSplit(pat, '\n', &numberPat);
 
     //Inizio programma con MPI
-
     //Creazione dei processi
     MPI_Init(NULL, NULL);
+    double start = MPI_Wtime();
+    int my_rank, comm_sz, volte;
     MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
-    
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
     //Misurazione per il tempo
-    double start, end;
-    start = MPI_Wtime();
+    double end;
     //Idea: suddividere la lista di pattern in parti uguali da dividere ai vari processi 
-
-    
     // FUNZIONE CON MPI
-    
+    for(int i = 0; i < numberPat-1; i += comm_sz){
+        volte=0;
+        if(i+my_rank>=numberPat-1 || !listaPat[i+my_rank]){
+            continue;
+        }
+        KMPSearchInt(listaPat[i+my_rank], txt, &volte);
+        printf("Ricercato \"%s\" dal processo %d di %d: %d ricorrenze trovate\n", listaPat[i+my_rank], my_rank+1, comm_sz, volte);
+    }
     end = MPI_Wtime();
     //stampa finale del tempo messo
+    double my_time = end-start, maximumTime=0;
+    
+    MPI_Reduce(&my_time, &maximumTime, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+    if (my_rank==0){
+        printf("\nTime: %f\n", maximumTime);
+    }
     MPI_Finalize();
-    printf("\nTime: %f\n", end - start);
+
     return 0;
 }
